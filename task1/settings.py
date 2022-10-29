46c0d70c518e560d7f5863828463b088c1dc4c65
ibukun-brain <78442733+ibukun-brain@users.noreply.github.com>
2022-10-29 03:07:04 +0100

    update

--- task1/settings.py
+++ task1/settings.py
@@ -144,5 +144,5 @@ STATICFILES_DIR = [
 SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_env_variable('DEBUG')
-CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ["hng9-production-086f.up.railway.app"]
+CORS_ALLOW_ALL_ORIGINS = True
